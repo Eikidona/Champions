55d3848ccffe7df2920a3066d13fb2cea756bf23
Hypocisy <1137943609@qq.com>
2024-07-30 00:31:30 +0800

    it seems deps are not incompatible with old code, so I decide to template remove, will write future. fix missing egg texture.

--- src/main/java/top/theillusivec4/champions/common/util/ChampionData.java
+++ src/main/java/top/theillusivec4/champions/common/util/ChampionData.java
@@ -10,7 +10,6 @@ import top.theillusivec4.champions.api.AffixCategory;
 import top.theillusivec4.champions.api.IAffix;
 import top.theillusivec4.champions.api.IChampion;
 import top.theillusivec4.champions.common.affix.core.AffixManager;
-import top.theillusivec4.champions.common.integration.scalinghealth.ScalingHealthPlugin;
 import top.theillusivec4.champions.common.rank.Rank;
 import top.theillusivec4.champions.common.rank.RankManager;
 
@@ -150,9 +149,9 @@ public class ChampionData {
       }
       float chance = rank.getChance();
 
-      if (Champions.scalingHealthLoaded) {
-        chance += (float) ScalingHealthPlugin.getSpawnIncrease(rank.getTier(), livingEntity);
-      }
+//      if (Champions.scalingHealthLoaded) {
+//        chance += (float) ScalingHealthPlugin.getSpawnIncrease(rank.getTier(), livingEntity);
+//      }
 
       if (RAND.nextFloat() < chance) {
         result = rank;
