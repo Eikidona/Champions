0dec350f3892cdb809f8204e2efb852b774869ed
Hypocisy <1137943609@qq.com>
2024-09-01 01:44:49 +0800

    use codec handel loot condition, remove RegistryReference.java, use neoforge registry system registry loot condition instead of mojang registry.

--- src/main/java/top/theillusivec4/champions/common/loot/ChampionPropertyCondition.java
+++ src/main/java/top/theillusivec4/champions/common/loot/ChampionPropertyCondition.java
@@ -1,9 +1,15 @@
 package top.theillusivec4.champions.common.loot;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.gson.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.advancements.critereon.MinMaxBounds;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.entity.Entity;
@@ -11,10 +17,12 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+import net.neoforged.neoforge.common.util.NeoForgeExtraCodecs;
 import top.theillusivec4.champions.api.IAffix;
 import top.theillusivec4.champions.api.IChampion;
 import top.theillusivec4.champions.common.capability.ChampionAttachment;
 import top.theillusivec4.champions.common.rank.Rank;
+import top.theillusivec4.champions.common.registry.ChampionsRegistry;
 
 import javax.annotation.Nonnull;
 import java.util.HashSet;
@@ -22,12 +30,15 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record LootItemChampionPropertyCondition(LootContext.EntityTarget target,
-                                                MinMaxBounds.Ints tier, AffixesPredicate affixes)
+public record ChampionPropertyCondition(LootContext.EntityTarget target,
+                                        MinMaxBounds.Ints tier, AffixesPredicate affixes)
   implements LootItemCondition {
 
-  public static final LootItemConditionType INSTANCE =
-    new LootItemConditionType(new ChampionConditionSerializer());
+  public static final Codec<ChampionPropertyCondition> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+    LootContext.EntityTarget.CODEC.fieldOf("target").forGetter(ChampionPropertyCondition::target),
+    MinMaxBounds.Ints.CODEC.fieldOf("tier").forGetter(ChampionPropertyCondition::tier),
+    AffixesPredicate.CODEC.fieldOf("affixes").forGetter(ChampionPropertyCondition::affixes)
+  ).apply(instance, ChampionPropertyCondition::new));
 
   @Nonnull
   @Override
@@ -53,14 +64,14 @@ public record LootItemChampionPropertyCondition(LootContext.EntityTarget target,
   @Nonnull
   @Override
   public LootItemConditionType getType() {
-    return INSTANCE;
+    return ChampionsRegistry.CHAMPION_PROPERTIES.get();
   }
 
-  public static class ChampionConditionSerializer
-    implements Serializer<LootItemChampionPropertyCondition> {
+  /*public static class ChampionConditionSerializer
+    implements Serializer<ChampionPropertyCondition> {
 
     @Override
-    public void serialize(final JsonObject json, final LootItemChampionPropertyCondition value,
+    public void serialize(final JsonObject json, final ChampionPropertyCondition value,
                           final JsonSerializationContext context) {
       json.add("tier", value.tier.serializeToJson());
       json.add("affixes", value.affixes.serializeToJson());
@@ -69,40 +80,28 @@ public record LootItemChampionPropertyCondition(LootContext.EntityTarget target,
 
     @Nonnull
     @Override
-    public LootItemChampionPropertyCondition deserialize(JsonObject json, @Nonnull
-      JsonDeserializationContext context) {
+    public ChampionPropertyCondition deserialize(JsonObject json, @Nonnull
+    JsonDeserializationContext context) {
       MinMaxBounds.Ints tier = MinMaxBounds.Ints.fromJson(json.get("tier"));
       AffixesPredicate affixes = AffixesPredicate.fromJson(json.get("affixes"));
-      return new LootItemChampionPropertyCondition(
+      return new ChampionPropertyCondition(
         GsonHelper.getAsObject(json, "entity", context, LootContext.EntityTarget.class), tier,
         affixes);
     }
-  }
+  }*/
 
   private record AffixesPredicate(Set<String> values, MinMaxBounds.Ints matches,
                                   MinMaxBounds.Ints count) {
 
+    public static final Codec<AffixesPredicate> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+      NeoForgeExtraCodecs.setOf(Codec.STRING).fieldOf("values").forGetter(AffixesPredicate::values),
+      MinMaxBounds.Ints.CODEC.fieldOf("matches").forGetter(AffixesPredicate::matches),
+      MinMaxBounds.Ints.CODEC.fieldOf("count").forGetter(AffixesPredicate::count)
+    ).apply(instance, AffixesPredicate::new));
     private static final AffixesPredicate ANY =
       new AffixesPredicate(new HashSet<>(), MinMaxBounds.Ints.ANY, MinMaxBounds.Ints.ANY);
 
-    private boolean matches(List<IAffix> input) {
-
-      if (this.values.isEmpty()) {
-        return this.count.matches(input.size());
-      } else {
-        Set<String> affixes = input.stream().map(IAffix::getIdentifier).collect(Collectors.toSet());
-        int found = 0;
-
-        for (String affix : this.values) {
-
-          if (affixes.contains(affix)) {
-            found++;
-          }
-        }
-        return this.matches.matches(found) && this.count.matches(input.size());
-      }
-    }
-
+    @Deprecated
     private static AffixesPredicate fromJson(JsonElement json) throws CommandSyntaxException {
 
       if (json != null && !json.isJsonNull()) {
@@ -148,11 +147,30 @@ public record LootItemChampionPropertyCondition(LootContext.EntityTarget target,
       return ANY;
     }
 
+    private boolean matches(List<IAffix> input) {
+
+      if (this.values.isEmpty()) {
+        return this.count.matches(input.size());
+      } else {
+        Set<String> affixes = input.stream().map(IAffix::getIdentifier).collect(Collectors.toSet());
+        int found = 0;
+
+        for (String affix : this.values) {
+
+          if (affixes.contains(affix)) {
+            found++;
+          }
+        }
+        return this.matches.matches(found) && this.count.matches(input.size());
+      }
+    }
+
+    @Deprecated
     public JsonElement serializeToJson() {
       if (this.values.isEmpty() && this.count.isAny() && this.matches.isAny()) {
         return JsonNull.INSTANCE;
       } else {
-        JsonObject jsonObject = new JsonObject();
+//        JsonObject jsonObject = new JsonObject();
         JsonArray jsonArray = new JsonArray();
 
         for (String value : this.values) {
@@ -164,10 +182,10 @@ public record LootItemChampionPropertyCondition(LootContext.EntityTarget target,
         if (min != null && min == 1 && max == null) {
           return jsonArray;
         }
-        jsonObject.add("values", jsonArray);
-        jsonObject.add("matches", this.matches.serializeToJson());
-        jsonObject.add("count", this.count.serializeToJson());
-        return jsonObject;
+//        jsonObject.add("values", jsonArray);
+//        jsonObject.add("matches", this.matches.serializeToJson());
+//        jsonObject.add("count", this.count.serializeToJson());
+        return CODEC.encodeStart(JsonOps.INSTANCE, new AffixesPredicate(values, matches, count)).get().left().orElse(JsonNull.INSTANCE);
       }
     }
   }
